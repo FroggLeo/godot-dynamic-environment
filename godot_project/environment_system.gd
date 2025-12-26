faf6c7c40513fc1a7cfad921ab3478f35e8d539b
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-26 15:57:51 -0800

    begin rework on high level gdscript controls

--- godot_project/environment_system.gd
+++ godot_project/environment_system.gd
@@ -7,30 +7,19 @@ extends Node3D
 # north / south
 
 @export var paused: bool = false
-## length of one day in seconds
-@export var day_length: float = 120
 
-## tilt variance of the sun and moon based on season
-# TODO add seasonal tilt
-@export var axial_tilt: float = 23.5:
-	set (value):
-		axial_tilt = value
-		_update_sun_moon()
-## latitude / base tilt
-@export_range(-90,90,0.01) var latitude: float = 34.05:
-	set (value):
-		latitude = value
-		_update_sun_moon()
+@export var environment: WorldEnvironment
+@export_range(0.0, 1.0, 0.001) var humidity := 0.35: set = _set_humidity
+@export_range(0.0, 500.0, 1.0) var aqi := 35.0: set = _set_aqi
+@export_range(1.0, 50.0, 0.1) var visibility_km := 30.0: set = _set_visibility
 
-@export_range(0,1,0.001) var time: float = 0:
-	set (value):
-		time = value
-		_update_sun_moon()
+@export_range(-30.0, 45.0, 0.1) var temperature_c := 30.0: set = _set_temperature
+@export_range(150.0, 450.0, 1.0) var ozone_du := 300.0: set = _set_ozone
 
-@onready var Sun := $Sun # node3d of sun
-@onready var SunLight := $Sun/SunLight # directional light of sun
-@onready var Moon := $Moon # node3d of moon
-@onready var MoonLight := $Moon/MoonLight # directional light of moon
+@export_range(0.5, 99999.0, 0.01) var camera_altitude := 0.5: set = _set_altitude
+
+@onready var Sun := $SunLight # directional light of sun
+@onready var Moon := $MoonLight # directional light of moon
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,29 +29,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if paused:
 		return
-	_update_time(delta)
-	_update_sun_moon()
 	
 
-# update the time
-func _update_time(delta: float) -> void:
-	time += delta / day_length
-	if time >= 1.0:
-		time -= 1.0
-
-func _update_sun_moon() -> void:
-	var angle := time * TAU
-	# set the sun rotation
-	Sun.global_rotation = Vector3(
-		angle,
-		0.0,
-		deg_to_rad(latitude)
-	)
-	# add pi to set to exactly opposite
-	Moon.rotation = Sun.rotation + Vector3(PI, 0.0, 0.0)
-	
-	var sun_dir := _get_light_dir(SunLight)
-	var moon_dir := _get_light_dir(MoonLight)
-	
 func _get_light_dir(light: DirectionalLight3D) -> Vector3:
 	return (-light.global_transform.basis.z).normalized()
+
+func _set_humidity(value): humidity = value
+func _set_aqi(value): aqi = value
+func _set_visibility(value): visibility_km = value
+func _set_temperature(value): temperature_c = value
+func _set_ozone(value): ozone_du = value
+func _set_altitude(value): camera_altitude = value
