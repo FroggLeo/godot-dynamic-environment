c6a90ceb14125a23144307447232fd3028b867c1
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-26 18:45:04 -0800

    add hue shift function and need to redo high level controls

--- godot_project/environment_system.gd
+++ godot_project/environment_system.gd
@@ -45,37 +45,40 @@ func _resolve_sky_material() -> void:
 
 func _update_atmosphere() -> void:
 	if not mat:
-		print("noooooooo")
 		return
 	# normalize to 0..1 as needed
-	var hu := humidity
+	var hu: float = clamp(humidity, 0, 1)
 	# 0..500 to 0..1
 	var aq: float = clamp(aqi / 500, 0, 1)
 	# 0..50 to 1..0
 	var vi: float = clamp((50 - visibility_km) / 50, 0, 1)
-	# get a nice haze out of all 3 values
-	# idea is adapted from aerosol optical depth
-	# visibility has the most effect, then aqi, then humidity has the smallest
-	var haze: float = pow(clamp(0.55 * vi + 0.30 * aq + 0.25 * hu, 0, 1), 1.4)
+	# -35..45 to 0..1
+	var te: float = clamp((temperature_c + 35) / 80, 0, 1)
+	# 150..450 to 0..1
+	var oz: float = clamp((ozone_du - 150) / 300, 0, 1)
+	# as aqi increases
+	# affects rayleigh, mie, mie g, density
+	# super yellow/orange
+	# aqi 500 ~ 8.0, 8.0, 0.85, 2.0
+	# as 
+	var rayleigh_strength: float = lerp(0.7, 1.7, )
+	# mie
 	var mie_strength: float = lerp(0.2, 5.0, haze);
 	# just put in the clean air mie g and polluted mie g
 	var mie_g: float = lerp(0.76, 0.93, clamp(haze * 0.9 + hu * 0.2, 0, 1))
-	# rayleigh strength drops as haze increases
-	var rayleigh_strength: float = lerp(1.3, 0.7, clamp(aq * 0.9 + haze * 0.4, 0, 1))
 	# ozone
-	# 150..450 to 0..1
-	var oz: float = clamp((ozone_du - 150) / 300, 0, 1)
 	var ozone_strength: float = lerp(0.8, 1.2, oz)
 	# temperature effect on haze
-	# -35..45 to 0..1
-	var te: float = clamp((temperature_c + 35) / 80, 0, 1)
 	mie_strength *= lerp(0.9, 1.1, te)
+	# atmosphere density increases as haze increase
+	var atm_density: float = lerp(0.6, 2.3, haze)
 	# update shader now
 	mat.set_shader_parameter("camera_altitude", camera_altitude)
 	mat.set_shader_parameter("rayleigh_strength", rayleigh_strength)
 	mat.set_shader_parameter("mie_strength", mie_strength)
 	mat.set_shader_parameter("mie_g", mie_g)
 	mat.set_shader_parameter("ozone_strength", ozone_strength)
+	mat.set_shader_parameter("atm_density", atm_density)
 
 func _get_light_dir(light: DirectionalLight3D) -> Vector3:
 	return (-light.global_transform.basis.z).normalized()
